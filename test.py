edd4045c0eaa5b7b9453b0e146581d4b45bd84fa
amit <amitchapde2@gmail.com>
2022-04-11 15:54:31 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+from pprint import pp
+
+
+def Binarytodecimal(n):
+    num=n;
+    dvalue=0;
+
+    base=1 
+    temp=num;
+    while(temp):
+        last_digit=temp%10
+        temp=int(temp/10)
+        dvalue+=last_digit*base
+        base*=2
+    return dvalue
+
+a=Binarytodecimal(101)
+print(a)
+print(11)
+print(12)
+print(10)
